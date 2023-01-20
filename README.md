50edbe78fae0774d016c2dbc3822b5b298483f5e
EpicYoshiMaster <epicyoshim@gmail.com>
2023-01-20 00:10:08 -0600

    Mod overhaul for tab support

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
-# CCPresetRevival
-This mod re-enables the Save Preset menu, which allows you to start the game on a specific part.
+# CCPresetRevivalPlus
+This mod re-enables an enhanced version of the Save Preset menu, which allows you to start the game from a specific part of the story.
+
+## Setting Up Presets
+Save Presets are found and stored in `<CrossCode Root Directory>/assets/data/save-presets`.
+
+The mod will look here for new presets that end in `.json`, ignoring the old base-game presets (ex. `0-before-boss.json`).
+
+**To make a new preset:**
+1. Create a new file ending in `.json` in the `save-presets` folder. For simplicity and organization, most presets are typically named `##-Preset-Title.json`, where `##` are 2 numbers to help keep presets in order. This is totally optional though, any name will work!
+2. Inside your file, you can use the following template:
+```json
+{
+    "title": {
+        "langUid": 1,
+        "en_US": "Preset Title",
+        "de_DE": "Preset Title",
+        "zh_CN": "Preset Title",
+        "ko_KR": "Preset Title",
+        "ja_JP": "Preset Title"
+    },
+    "sub": {
+        "langUid": 2,
+        "en_US": "Preset Subtitle",
+        "en_DE": "Preset Subtitle",
+        "zh_CN": "Preset Subtitle",
+        "de_DE": "Preset Subtitle",
+        "ko_KR": "Preset Subtitle",
+        "ja_JP": "Preset Subtitle"
+    },
+    "savefile": "Your save string goes here"
+}
+```
+
+Your preset will look something like this:
+![preset-example](https://user-images.githubusercontent.com/32598419/213625882-206df9ab-030e-47a0-9ac3-dd5113b31259.png)
+
+3. Replace the `title` and `sub` names with whatever you'd like for the preset you're trying to make!
+4. Place the appropriate save string for where you want the preset to start into the `savefile` property. You can get a save string by pressing **F10** while in-game. Click the giant string of text under "Current Save Slot Data" and copy that into `savefile`. You'll know it's a save string if it starts with `[-!_0_!-]`.
+5. You're done! You should be able to boot the game up with this mod and use your new preset!
+
+## Folder Support
+With Preset Revival Plus, you have the ability to use folders to help organize your huge mess of presets!
+
+Inside your presets folder (`<CrossCode Root Directory>/assets/data/save-presets`), you can place your presets in any number of layers of folders, but the top layer is the most important.
+
+The top folder layer (ex. `.../save-presets/MyPresetFolder/00-my-preset.json`) is used to divide presets into separate tabs in-game. This example preset would show under a tab called `MyPresetFolder`, along with any other presets in that folder (or folders in that folder containing more presets).
+
+The tab order as well as preset order are determined by the overall file order, so `.../save-presets/A/` will come before `.../save-presets/B/` and so on. The same ordering system applies to any sub-folders within those tabs.
+
+For convenience, if a tab folder is named `##-Folder Name` where `##` can be replaced with two numbers to keep things organized, the tab name in-game will show as just `Folder Name` instead.
+
+An example `save-presets` folder using Folder Support:
+![folders-example](https://user-images.githubusercontent.com/32598419/213628491-333f8b82-a58b-4cc1-8ca5-d1c0ec6e56b7.png)
+
+This would appear in-game as:
+![image](https://user-images.githubusercontent.com/32598419/213628758-c6ea3aac-53a3-4ead-b56b-277d8d66743e.png)
---
+++ prestart.js
@@ -0,0 +1,205 @@
+const NO_CATEGORY_NAME = "No Category";
+
+sc.TitleScreenButtonGui.inject({
+	init() {
+		this.parent();
+		this._createButton("preset", this.buttons.last().hook.pos.y + 39, 6, () => {
+			this.background.doStateTransition("DEFAULT");
+			this.presetMenu.activate();
+		}, "preset");
+	}
+});
+
+sc.SavePresetData.inject({
+	category: NO_CATEGORY_NAME,
+
+	init(filePath, presetCategory) {
+		this.parent(filePath);
+		this.category = presetCategory;
+	}
+});
+
+export class PresetFileCategory {
+	constructor(name, category) {
+		this.name = name;
+		this.category = category;
+	}
+}
+
+const oldNames = ["0-before-boss", "1-rhombus-dng-start", "2-continue-story", "3-autumn-rise", "4-apollo-duel", "5-before-bergen", "6-before-maroon", "7-fajro-temple", "8-autumns-fall"];
+
+sc.savePreset.slots = [];
+
+const presetFiles = findPresets('assets/data/save-presets/');
+for (const presetFile of presetFiles) {
+
+	const name = presetFile.name.replace('.json', '');
+	const category = presetFile.category;
+
+	if (oldNames.includes(name)) {
+		continue;
+	}
+
+	sc.savePreset.slots.push(new sc.SavePresetData(name, category));
+}
+
+function findPresets(dir, subDirName) {
+	const fs = require('fs');
+	const { resolve } = require('path');
+
+	const directoryItems = fs.readdirSync(dir);
+
+	let files = [];
+	for(const dirItem of directoryItems)
+	{
+		const itemPath = resolve(dir, dirItem);
+		const stat = fs.lstatSync(itemPath);
+
+		if(stat.isDirectory())
+		{
+			const categoryName = (dirItem.indexOf('-') == 2 && dirItem.length > 3) ? dirItem.substring(3) : dirItem; 
+			files = files.concat(findPresetsRecursive(itemPath, subDirName ? (subDirName + '/' + dirItem) : dirItem, categoryName));
+		}
+		else if(stat.isFile())
+		{
+			files.push(new PresetFileCategory(subDirName ? (subDirName + '/' + dirItem) : dirItem, NO_CATEGORY_NAME));
+		}
+	}
+
+	return files;
+}
+
+function findPresetsRecursive(dir, subDirName, category) {
+	const fs = require('fs');
+	const { resolve } = require('path');
+
+	const directoryItems = fs.readdirSync(dir);
+
+	let files = [];
+	for(const dirItem of directoryItems)
+	{
+		const itemPath = resolve(dir, dirItem);
+		const stat = fs.lstatSync(itemPath);
+
+		if(stat.isDirectory())
+		{
+			files = files.concat(findPresetsRecursive(itemPath, subDirName ? (subDirName + '/' + dirItem) : dirItem, category));
+		}
+		else if(stat.isFile())
+		{
+			files.push(new PresetFileCategory(subDirName ? (subDirName + '/' + dirItem) : dirItem, category));
+		}
+	}
+
+	return files;
+}
+
+sc.TitlePresetMenu.inject({
+	pageLeftButton: null,
+	pageRightButton: null,
+	categoryList: [],
+	currCategoryIndex: 0,
+
+	init(b, a) {
+		this.parent(b, a);
+		this.backButton.setWidth(136);
+
+		this.pageLeftButton = new sc.ButtonGui("\\i[arrow-left]", 34, true, sc.BUTTON_TYPE.SMALL);
+		this.pageLeftButton.setAlign(ig.GUI_ALIGN.X_RIGHT, ig.GUI_ALIGN.Y_BOTTOM);
+		this.pageLeftButton.setPos(34, 1);
+		this.pageLeftButton.keepMouseFocus = true;
+		this.pageLeftButton.submitSound = sc.BUTTON_SOUND.submit;
+		this.pageLeftButton.onButtonPress = function() {
+			this.setCategoryIndex(this.currCategoryIndex - 1);
+		}.bind(this);
+		this.buttonInteract.addGlobalButton(this.pageLeftButton, this.onLeftPressCheck.bind(this));
+		this.addChildGui(this.pageLeftButton);
+
+		this.pageRightButton = new sc.ButtonGui("\\i[arrow-right]", 34, true, sc.BUTTON_TYPE.SMALL);
+		this.pageRightButton.setAlign(ig.GUI_ALIGN.X_RIGHT, ig.GUI_ALIGN.Y_BOTTOM);
+		this.pageRightButton.setPos(0, 1);
+		this.pageRightButton.keepMouseFocus = true;
+		this.pageRightButton.submitSound = sc.BUTTON_SOUND.submit;
+		this.pageRightButton.onButtonPress = function() {
+			this.setCategoryIndex(this.currCategoryIndex + 1);
+		}.bind(this);
+		this.buttonInteract.addGlobalButton(this.pageRightButton, this.onRightPressCheck.bind(this));
+		this.addChildGui(this.pageRightButton);
+
+		this.categoryList = [];
+
+		for(let i = 0; i < sc.savePreset.slots.length; i++) {
+			const presetCategory = sc.savePreset.slots[i].category;
+
+			if(!this.categoryList.includes(presetCategory)) {
+				this.categoryList.push(presetCategory);
+			}
+		}
+
+		if(this.categoryList.length > 0) {
+			this.header.setText(this.categoryList[this.currCategoryIndex]);
+		}
+		else {
+			this.header.setText(ig.lang.get("sc.gui.title-screen.preset-empty"));
+		}
+	},
+	update() {
+		this.parent();
+	},
+	setCategoryIndex(index) {
+		let newIndex = index;
+
+		if(newIndex >= this.categoryList.length) {
+			newIndex = 0;
+		}
+
+		if(newIndex < 0) {
+			newIndex = this.categoryList.length - 1;
+		}
+
+		if(newIndex === this.currCategoryIndex) return;
+
+		this.currCategoryIndex = newIndex;
+
+		if(this.categoryList.length > 0) {
+			this.header.setText(this.categoryList[this.currCategoryIndex]);
+		}
+		else {
+			this.header.setText(ig.lang.get("sc.gui.title-screen.preset-empty"));
+		}
+
+		this.createList();
+	},
+	onLeftPressCheck() {
+		return sc.control.menuCircleLeft()
+	},
+	onRightPressCheck() {
+		return sc.control.menuCircleRight()
+	},
+	createList: function() {
+		var b = 0,
+			a = 0,
+			b = this.itemList.list.buttonGroup.current.y,
+			a = -this.itemList.list.box.hook.scroll.y;
+
+		this.itemList.list.buttonGroup.clear();
+		this.itemList.list.clear(false);
+		this.slots.length = 0;
+
+		if(this.categoryList.length <= 0) return;
+
+		for (let index = 0; index < sc.savePreset.slots.length; index++) {
+			if(sc.savePreset.slots[index].category !== this.categoryList[this.currCategoryIndex]) continue;
+
+			let newPresetButton = new sc.TitlePresetMenu.SaLoButton(index, sc.savePreset.slots[index]);
+			newPresetButton.setSize(201, 44);
+			this.itemList.addButton(newPresetButton);
+			this.slots[index] = newPresetButton;
+		}
+
+		b = Math.max(0, Math.min(b, this.itemList.list.getChildren().length));
+		this.itemList.list._prevIndex = b;
+		ig.input.mouseGuiActive ? this.itemList.list.buttonGroup.setCurrentFocus(0, b) : this.itemList.list.buttonGroup.focusCurrentButton(0, b, false, true);
+		this.itemList.list.scrollToY(a, true)
+	}
+});
